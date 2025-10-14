a8dc5a0400903dc0cdf608f2d756c05c76d0d6ef
borgmon <borgmon7@gmail.com>
2025-10-13 19:46:42 -0700

    remove extra in build branch

--- src/slackware_pkg/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Slackware Package Builder - Build packages from source into Slackware format."""
-
-from .builder import SlackwarePackageBuilder
-from .models import BuildConfig, Package
-
-__version__ = "0.1.0"
-__all__ = ["SlackwarePackageBuilder", "Package", "BuildConfig"]
